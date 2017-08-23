631dae113eab2ae946e915fc7cf050696d9de0c7
facet-it <facet.it.be@gmail.com>
2017-08-23 22:33:41 +0200

    added my first stupid component

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { Output } from './output/output.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Output
   ],
   imports: [
     BrowserModule
---
+++ my-first-app/src/app/output/output.component.ts
@@ -0,0 +1,15 @@
+import {Component} from '@angular/core';
+
+@Component({
+ selector: 'fix-output',
+ template: `
+    <p> non angular output<\p>
+    <p> field output: <\p>
+    <p> method output<\p>
+ `
+})
+/* Case sensitive!! */
+export class Output{
+
+
+}
